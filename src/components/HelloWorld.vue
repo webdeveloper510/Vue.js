8829d6672b5434957ef8137183a3d3debab46f00
Rekha Thakur <rekhathakur@datepalm.media>
2022-07-30 02:44:45 +0530

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,6 +11,7 @@ export default {
       details3: [],
       details4: [],
       details5: [],
+       details6: [],
       CS_DATA_LIST: [],
       filterResults: [],
       SCHOLARSHIPS: [],
@@ -23,7 +24,7 @@ export default {
       course1: [],
       units: [],
       modeValue: [],
-      shiftValue:[],
+      shiftValue: [],
       status: Boolean = false,
     }
   },
@@ -44,6 +45,7 @@ export default {
       responseData.scholarships.map(async (t) => {
         if ((this.SCHOLARSHIPS.push(t), this.CS_DATA_LIST.length > 0)) {
           let e = this.CS_DATA_LIST.find((e) => e.course.name === t.course.name && e.school.name === t.school.name);
+
           if (null == e) {
             let e = { course: t.course, school: t.school, content_level: t.content_level, modalities: [], shifts: [], units: [], scholarships: [] };
             this.CS_DATA_LIST.push(e);
@@ -63,9 +65,35 @@ export default {
         });
 
       this.details = this.CS_DATA_LIST
-      // this.details5 = this.CS_DATA_LIST
-      // console.log(this.details)
       this.getMode()
+
+    },
+    getData1() {
+      responseData.scholarships.map(async (t) => {
+        if ((this.SCHOLARSHIPS.push(t), this.details5.length > 0)) {
+          let e = this.details5.find((e) => e.course.name === t.course.name && e.school.name === t.school.name);
+
+          if (null == e) {
+            let e = { course: t.course, school: t.school, content_level: t.content_level, modalities: [], shifts: [], units: [], scholarships: [] };
+            this.details5.push(e);
+          }
+        } else {
+          let e = { course: t.course, school: t.school, content_level: t.content_level, modalities: [], shifts: [], units: [], scholarships: [] };
+          this.details5.push(e);
+        }
+      }),
+
+        responseData.scholarships.map(async (t) => {
+          const e = await t;
+          var shifts = [], modalities = [], units = []
+          for (let t = 0; t < this.details5.length; t++) {
+            this.details5[t].course.name === e.course.name && this.details5[t].school.name === e.school.name && this.details5[t].content_level.name === e.content_level.name && this.details5[t].scholarships.push(e);
+
+          }
+        });
+      this.details3 = this.details5
+      console.log(this.details3)
+      // this.getMode()
 
     },
     async getMode() {
@@ -96,7 +124,7 @@ export default {
         this.getModalities(await this.CS_DATA_LIST[i].scholarships, i)
         this.getShifts(await this.CS_DATA_LIST[i].scholarships, i)
         this.getUnitss(await this.CS_DATA_LIST[i].scholarships, i)
-        
+
       }
     },
     getModalities(t, index) {
@@ -125,7 +153,7 @@ export default {
       this.CS_DATA_LIST[index].shifts.push(...a)
       console.log(this.CS_DATA_LIST)
     },
-     getUnitss(t, index) {
+    getUnitss(t, index) {
       let e = "",
         a = [];
 
@@ -137,38 +165,47 @@ export default {
       })
 
       this.CS_DATA_LIST[index].units.push(...a)
-      
+
     },
     changeModalities(data1, index) {
       var filterData = {
-        scholarships:[]
+        scholarships: []
       }
-      this.modeValue=[]
-      this.modeValue.push(data1.name)
-      console.log(this.CS_DATA_LIST[index].scholarships  )
-      this.CS_DATA_LIST[index].scholarships.filter((data, index) => {
-        console.log('i am here',data)
-        if (data.content_modality.name == data1.name) {
-          return filterData.scholarships.push(data)
+      if (this.modeValue.length > 0 && this.shiftValue.length > 0) {
+        location.reload()
+      }
+      else {
+        var filter1 = []
+        filter1 = this.details5
+        this.CS_DATA_LIST[index].scholarships = this.details5[index].scholarships
+        this.modeValue = []
+        this.modeValue.push(data1.name)
+        console.log(this.CS_DATA_LIST)
+        this.CS_DATA_LIST[index].scholarships.filter((data, index) => {
+          // console.log('i am here',data)
+          if (data.content_modality.name == data1.name) {
+            return filterData.scholarships.push(data)
+          }
         }
+        )
+        this.CS_DATA_LIST[index]['scholarships'] = filterData.scholarships
+        this.details = filterData.scholarships
+        console.log('filterData', this.details)
       }
-      )
-      console.log(index,filterData)
-    // this.CS_DATA_LIST[index]['scholarships'] = filterData.scholarships
-    this.details=filterData.scholarships
-      console.log('filterData',this.details)
+
+
     },
-    changeShifts(data2,index) {
+    changeShifts(data2, index) {
       var filterData = []
-      var filter1=[]
-      this.shiftValue=[]
-       this.shiftValue.push(data2.name)
-      filter1=this.details
+      var filter1 = []
+      this.shiftValue = []
+      this.shiftValue.push(data2.name)
+      filter1 = this.details
       delete filter1.scholarships
-      console.log('filter',filter1)
-      this.CS_DATA_LIST[index].scholarships=filter1
-      console.log(this.CS_DATA_LIST,index)
-      if (this.modeValue.length ==0 ) {
+      console.log('filter', filter1)
+      this.CS_DATA_LIST[index].scholarships = filter1
+      console.log(this.CS_DATA_LIST, index)
+      if (this.modeValue.length == 0) {
         alert('selecione a modalidade')
       }
       else {
@@ -181,21 +218,21 @@ export default {
         }
         )
         console.log(filterData)
-         this.CS_DATA_LIST[index]['scholarships']=filterData
-         this.details5=this.CS_DATA_LIST[index]['scholarships']
-         console.log(this.details5)
+        this.CS_DATA_LIST[index]['scholarships'] = filterData
+        this.details5 = this.CS_DATA_LIST[index]['scholarships']
+        console.log(this.details5)
         // this.details=filterData;
       }
 
     },
-    changeUnits(data3,index) {
-       var filterData = []
-      var filter1=[]
-      filter1=this.details
+    changeUnits(data3, index) {
+      var filterData = []
+      var filter1 = []
+      filter1 = this.details
       delete filter1.scholarships
-      this.CS_DATA_LIST[index].scholarships=filter1
-      console.log(this.CS_DATA_LIST,index)
-      if (this.modeValue.length ==0 ) {
+      this.CS_DATA_LIST[index].scholarships = filter1
+      console.log(this.CS_DATA_LIST, index)
+      if (this.modeValue.length == 0) {
         alert('selecione a modalidade')
       }
       else {
@@ -208,43 +245,77 @@ export default {
         }
         )
         console.log(filterData)
-         this.CS_DATA_LIST[index]['scholarships']=filterData
-           this.details5=this.CS_DATA_LIST[index]['scholarships']
-            console.log(this.details5.length)
-          // this.details=filterData;
+        this.CS_DATA_LIST[index]['scholarships'] = filterData
+        this.details5 = this.CS_DATA_LIST[index]['scholarships']
+        console.log(this.details5.length)
+        // this.details=filterData;
       }
     },
-    modeChange(event) {
-      this.filterResults = this.details
+    async modeChange(event) {
+      let filterResults =[] 
+      filterResults= this.CS_DATA_LIST
       if (event.target.value != 'Ver Todos') {
-        var filterData = [];
-        this.details.filter((data, index) => {
-          if (data.content_modality.name == event.target.value) {
-            return filterData.push(data)
+        this.details4=filterResults
+        this.details4.map(async (t,index) => {
+          const e = await t;
+          console.log(e)
+          let scholarship=[]
+          var shifts = [], modalities = [], units = []
+          for (let t = 0; t < this.details4[index].scholarships.length; t++) {
+             console.log(this.details4[index].scholarships[t].content_modality.name===event.target.value)
+             if(this.details4[index].scholarships[t].content_modality.name===event.target.value){
+              // scholarship.push(this.details4[index].scholarships[t])
+              // this.details6[index]=this.details4[index]
+               scholarship.push(this.details4[index].scholarships[t])
+             }
+            
           }
-        }
-        )
-        // this.details4['scholarships'] = filterData
-        // this.details = filterData;
-      }
-      else {
-        this.details.scholarships = responseData.scholarships
-        this.details4['scholarships'] = responseData.scholarships
+          if(scholarship.length>0){
+            delete t.scholarships
+            this.details6[index]=t
+             this.details6[index]['scholarships']=scholarship
+          }
+          else{
+             this.details6[index]=[]
+          }
+                    console.log(this.details6)
+       
+        });
+        
       }
     },
     modeChange1(event) {
-      console.log(this.details4)
-      var filterData = [];
+      console.log(this.details6)
+      let dataValue=[]
+      let filterData=[];
+      dataValue=this.details6
       if (event.target.value != 'Ver Todos') {
-        if (this.details4.scholarships == undefined) {
-          responseData.scholarships.filter((data, index) => {
-            if (data.content_shift.name == event.target.value) {
-              return filterData.push(data)
-            }
-            this.details.scholarships = filterData
-            console.log(this.details)
+        if (this.details6.length > 0) {
+          this.details6.map(async (t,index) => {
+          const e = await t;
+          console.log(e)
+          let scholarship=[]
+          var shifts = [], modalities = [], units = []
+          for (let t = 0; t < dataValue[index].scholarships.length; t++) {
+             console.log(dataValue[index].scholarships[t].content_shift.name===event.target.value)
+             if(dataValue[index].scholarships[t].content_shift.name===event.target.value){
+              // scholarship.push(this.details4[index].scholarships[t])
+              // this.details6[index]=this.details4[index]
+               scholarship.push(dataValue[index].scholarships[t])
+             }
+            
           }
-          )
+          if(scholarship.length>0){
+            delete t.scholarships
+            filterData[index]=t
+             filterData[index]['scholarships']=scholarship
+          }
+          else{
+           console.log( filterData[index])
+          }
+                    console.log(filterData)
+       
+        });
         }
         else {
           this.details4.scholarships.filter((data, index) => {
@@ -262,8 +333,6 @@ export default {
 
       }
       else {
-
-        console.log('this is working12', this.details4)
         this.details = this.details4
         console.log(this.details)
 
@@ -289,6 +358,7 @@ export default {
   },
   mounted() {
     this.getData()
+    this.getData1()
   }
 }
 </script>
@@ -386,9 +456,10 @@ export default {
                   <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
                     <div v-for="data in shift1" :key='data' class="mx-1">
                       <input type="radio" class="btn-check" name="btnradio1" v-bind:id="data" autocomplete="off">
-                      <label class="btn btn-outline-success btn-sm" v-bind:for="data" @click="changeShifts(data,index)">{{
-                          data
-                      }}</label>
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data"
+                        @click="changeShifts(data, index)">{{
+                            data
+                        }}</label>
                     </div>
                   </div>
                   <br />
@@ -397,8 +468,9 @@ export default {
                     style="flex-wrap:wrap">
                     <div v-for="data in units" :key='data' class="mx-1">
                       <input type="radio" class="btn-check" name="btnradio2" v-bind:id="data" autocomplete="off">
-                      <label class="btn btn-outline-success btn-sm" v-bind:for="data" @click="changeUnits(data,index)">{{ data
-                      }}</label>
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data"
+                        @click="changeUnits(data, index)">{{ data
+                        }}</label>
                     </div>
                   </div>
 
@@ -487,9 +559,9 @@ export default {
 
                   <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
                     <div v-for="data in data.modalities" :key='data' class="mx-1">
-                      <input type="radio" class="btn-check" name="btnradio" v-bind:id="data.name+index"
+                      <input type="radio" class="btn-check" name="btnradio" v-bind:id="data.name + index"
                         autocomplete="off">
-                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name+index"
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name + index"
                         @click="changeModalities(data, index)">{{
                             data.name
                         }}</label>
@@ -500,10 +572,10 @@ export default {
                   <label class="mt-3">Shifts :</label> <br />
                   <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
                     <div v-for="data in data.shifts" :key='data' class="mx-1">
-                      <input type="radio" class="btn-check" name="btnradio1" v-bind:id="data.name+index"
+                      <input type="radio" class="btn-check" name="btnradio1" v-bind:id="data.name + index"
                         autocomplete="off">
-                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name+index"
-                        @click="changeShifts(data,index)">{{
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name + index"
+                        @click="changeShifts(data, index)">{{
                             data.name
                         }}</label>
                     </div>
@@ -513,10 +585,10 @@ export default {
                   <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group"
                     style="flex-wrap:wrap">
                     <div v-for="data in data.units" :key='data' class="mx-1">
-                      <input type="radio" class="btn-check" name="btnradio2" v-bind:id="data.name+index"
+                      <input type="radio" class="btn-check" name="btnradio2" v-bind:id="data.name + index"
                         autocomplete="off">
-                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name+index"
-                        @click="changeUnits(data,index)">{{ data.name
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name + index"
+                        @click="changeUnits(data, index)">{{ data.name
                         }}</label>
                     </div>
                   </div>
@@ -528,50 +600,50 @@ export default {
             <div class="my-4">
               <hr />
             </div>
-            <div >
-              <div v-if="data.scholarships.length>0">
-              <div class="payment" v-for="data1 in data.scholarships" :key='data1'>
-                <div class="row">
-                  <div class="col-md-9">
-                    <div class="row" >
-                      <div class="col-md-3" >
-                        <p>Modality: <b> {{ data1?.content_modality?.name }} </b><br />
-                          Shift:<b> {{ data1?.content_shift?.name }} </b><br />
-                          Unit:<b>{{ data1?.unit?.name }} </b></p>
-                      </div>
-                      <div class="col-md-3">
-                        <p>Type: <b> {{ data1?.content_type?.name }} </b><br />
-                          's Degree Duration: <br /> <b>{{ data1?.duration }} {{ data1?.duration_type }} </b><br />
-                          Installments:<b> {{ data1?.discount_installments }} </b></p>
-                      </div>
-                      <div class="col-md-3">
-                        <p class="p-0">Monthly payment: <br />
-                          <span class="frombrl">From BRL {{ data1?.original_price }}</span><br />
-                          For <b> BRL <br /><span class="brl">{{ data1?.comission_amount }}</span> </b>
-                        </p>
-                        <small class="text-center">8785865 VB163.28 - 100</small>
-                      </div>
-                      <div class="col-md-3">
-                        <p>Scholarship: <b> {{ data1?.percent }}%</b> <br />
-                          Total Savings:
-                          <b> BRL {{ data1?.economy_total }} </b>
-                        </p>
+            <div>
+              <div v-if="data.scholarships.length > 0">
+                <div class="payment" v-for="data1 in data.scholarships" :key='data1'>
+                  <div class="row">
+                    <div class="col-md-9">
+                      <div class="row">
+                        <div class="col-md-3">
+                          <p>Modality: <b> {{ data1?.content_modality?.name }} </b><br />
+                            Shift:<b> {{ data1?.content_shift?.name }} </b><br />
+                            Unit:<b>{{ data1?.unit?.name }} </b></p>
+                        </div>
+                        <div class="col-md-3">
+                          <p>Type: <b> {{ data1?.content_type?.name }} </b><br />
+                            's Degree Duration: <br /> <b>{{ data1?.duration }} {{ data1?.duration_type }} </b><br />
+                            Installments:<b> {{ data1?.discount_installments }} </b></p>
+                        </div>
+                        <div class="col-md-3">
+                          <p class="p-0">Monthly payment: <br />
+                            <span class="frombrl">From BRL {{ data1?.original_price }}</span><br />
+                            For <b> BRL <br /><span class="brl">{{ data1?.comission_amount }}</span> </b>
+                          </p>
+                          <small class="text-center">8785865 VB163.28 - 100</small>
+                        </div>
+                        <div class="col-md-3">
+                          <p>Scholarship: <b> {{ data1?.percent }}%</b> <br />
+                            Total Savings:
+                            <b> BRL {{ data1?.economy_total }} </b>
+                          </p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                  <div class="col-md-3 text-center">
-                    <a href="#">see details</a> <br />
-                    <button class="btn btn-success btn-sm" type="button">CHOOSE THIS BAG</button>
+                    <div class="col-md-3 text-center">
+                      <a href="#">see details</a> <br />
+                      <button class="btn btn-success btn-sm" type="button">CHOOSE THIS BAG</button>
+                    </div>
                   </div>
                 </div>
               </div>
+              <div v-if="data.scholarships.length == 0">
+                <p>Não há bolsas disponíveis com os itens selecionados, mude a unidade, turno ou modalidade selecionada.
+                </p>
               </div>
-               <div v-if="data.scholarships.length==0">
-              <p>Não há bolsas disponíveis com os itens selecionados, mude a unidade, turno ou modalidade selecionada.
-              </p>
             </div>
-            </div>
-         
+
           </div>
         </div>
         <!-- <div class="my-4">
