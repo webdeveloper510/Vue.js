0c6549d1a050a4bb09d7e66eeb75874fc5563a56
webdeveloper510 <developerweb6@gmail.com>
2022-08-19 23:32:23 +0530

    changes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -16,6 +16,7 @@ export default {
       filterResults: [],
       SCHOLARSHIPS: [],
       newData: [],
+      filterActive:false,
       mode: ['Ver Todos'],
       shift: ['Ver Todos'],
       course: ['Ver Todos'],
@@ -181,7 +182,8 @@ export default {
         this.modeValue = []
         this.modeValue.push(data1.name)
         console.log(this.CS_DATA_LIST)
-        this.CS_DATA_LIST[index].scholarships.filter((data, index) => {
+     if(this.filterActive==false){
+         this.CS_DATA_LIST[index].scholarships.filter((data, index) => {
           // console.log('i am here',data)
           if (data.content_modality.name == data1.name) {
             return filterData.scholarships.push(data)
@@ -190,6 +192,18 @@ export default {
         )
         this.CS_DATA_LIST[index]['scholarships'] = filterData.scholarships
         this.details = filterData.scholarships
+     }
+     else{
+         this.filterResults[index].scholarships.filter((data, index) => {
+          // console.log('i am here',data)
+          if (data.content_modality.name == data1.name) {
+            return filterData.scholarships.push(data)
+          }
+        }
+        )
+        this.filterResults[index]['scholarships'] = filterData.scholarships
+        this.details = filterData.scholarships
+     }
         console.log('filterData', this.details)
 
 
@@ -200,9 +214,11 @@ export default {
       this.shiftValue = []
       this.shiftValue.push(data2.name)
       console.log(this.CS_DATA_LIST, index)
-      if (this.modeValue.length == 0) {
+  if(this.filterActive==false){
+        if (this.modeValue.length == 0) {
         alert('selecione a modalidade')
       }
+      
       else {
         console.log(this.CS_DATA_LIST)
         this.CS_DATA_LIST[index].scholarships.filter((data, index) => {
@@ -218,7 +234,27 @@ export default {
         console.log(this.details5)
         // this.details=filterData;
       }
+  }
+else{
+      if (this.modeValue.length == 0) {
+        alert('selecione a modalidade')
+      }
+      
+      else {
+        this.filterResults[index].scholarships.filter((data, index) => {
+          if (data.content_shift.name == data2.name && data.content_modality.name == this.modeValue[0]) {
+            return filterData.push(data)
 
+          }
+        }
+        )
+        console.log(filterData)
+        this.filterResults[index]['scholarships'] = filterData
+        this.details5 = this.filterResults[index]['scholarships']
+        console.log(this.details5)
+        // this.details=filterData;
+      }
+}
     },
     changeUnits(data3, index) {
       var filterData = []
@@ -227,7 +263,8 @@ export default {
       delete filter1.scholarships
       this.CS_DATA_LIST[index].scholarships = filter1
       console.log(this.CS_DATA_LIST, index)
-      if (this.modeValue.length == 0) {
+  if(this.filterActive==false){
+        if (this.modeValue.length == 0) {
         alert('selecione a modalidade')
       }
       else {
@@ -245,77 +282,71 @@ export default {
         console.log(this.details5.length)
         // this.details=filterData;
       }
+  }
+  else{
+       if (this.modeValue.length == 0) {
+        alert('selecione a modalidade')
+      }
+      else {
+        console.log(this.filterResults)
+        this.filterResults[index].scholarships.filter((data, index) => {
+          if (data.unit.name == data3.name && data.content_modality.name == this.modeValue[0] && data.content_shift.name == this.shiftValue[0]) {
+            return filterData.push(data)
+
+          }
+        }
+        )
+        console.log(filterData)
+        this.filterResults[index]['scholarships'] = filterData
+        this.details5 = this.filterResults[index]['scholarships']
+        console.log(this.details5.length)
+        // this.details=filterData;
+      }
+  }
     },
     //this one 
     modeChange(event) {
-     var filterResults=JSON.parse(JSON.stringify(this.CS_DATA_LIST));
+      this.filterActive=true;
+      this.filterResults=JSON.parse(JSON.stringify(this.CS_DATA_LIST));
     
-      console.log(filterResults)
+      console.log(this.filterResults)
     
  
       if (event.target.value != 'Ver Todos') {
        
-       filterResults.map((t,index) => {
-    
-         
-        
+       this.filterResults.map((t,index) => {    
            var scholarships =  this.filterScholarships(t.scholarships);
-          
-         
-         // console.log(scholarships)
-         
-          if(scholarships.length>0){
-       
-           filterResults[index]['scholarships'] = scholarships
-           
+      // console.log(scholarships)        
+          if(scholarships.length>0){ 
+           this.filterResults[index]['scholarships'] = scholarships
           }
           else{
-             filterResults[index]['scholarships'] = []
+             this.filterResults[index]['scholarships'] = []
           }
-                 
-       
-       
-     
       });
-       
-        
-
-       
-
-    console.log( filterResults)
+    console.log( this.filterResults)
+    // this.CS_DATA_LIST=filterResults
       }
         
     },
       filterScholarships(scholarships){
-       var filteredScholarships =  scholarships.filter((el)=>{
-          
-           
+       var filteredScholarships =  scholarships.filter((el)=>{  
             const flag = this.filterArray(el)   
-          
-              
-            return flag < 0  ? true : false
-                 
+            return flag < 0  ? true : false                
           });
       
           return filteredScholarships
     },
+
     filterArray(element,){
       var flagArray= []
       for (const [key, value] of Object.entries(this.selectedMode)) {
              for(const [key1,value1] of Object.entries(value)){
              if(this.selectedMode[key][key1]!=''){
-          
                   flagArray.push(this.selectedMode[key][key1]==element[key][key1])
-              
              }
-             }
-             
-
-
-                
+             }           
             }
-            
-        
             return flagArray.indexOf(false)
               
     },
@@ -325,48 +356,61 @@ export default {
       let filterData=[];
       dataValue=this.details6
       console.log(dataValue)
-      if (event.target.value != 'Ver Todos') {
-        if (this.details6.length > 0) {
-          this.details6.map(async (t,index) => {
-          const e = await t;
-          console.log(e)
-          let scholarship=[]
-          var shifts = [], modalities = [], units = []
-          for (let j = 0; j < dataValue[index].scholarships.length; j++) {
-             console.log(dataValue[index].scholarships[j].content_shift.name===event.target.value)
-             if(dataValue[index].scholarships[j].content_shift.name===event.target.value){
-              // scholarship.push(this.details4[index].scholarships[t])
-              // this.details6[index]=this.details4[index]
-               scholarship.push(dataValue[index].scholarships[j])
-             }
+      // if (event.target.value != 'Ver Todos') {
+        
+        // if (this.details6.length > 0) {
+        //   this.details6.map(async (t,index) => {
+        //   const e = await t;
+        //   console.log(e)
+        //   let scholarship=[]
+        //   var shifts = [], modalities = [], units = []
+        //   for (let j = 0; j < dataValue[index].scholarships.length; j++) {
+        //      console.log(dataValue[index].scholarships[j].content_shift.name===event.target.value)
+        //      if(dataValue[index].scholarships[j].content_shift.name===event.target.value){
+        //       // scholarship.push(this.details4[index].scholarships[t])
+        //       // this.details6[index]=this.details4[index]
+        //        scholarship.push(dataValue[index].scholarships[j])
+        //      }
             
-          }
-          if(scholarship.length>0){
-            delete t.scholarships
-            t['scholarships'] = scholarship
-            filterData.push(t)
-             //filterData[index]['scholarships']=scholarship
+        //   }
+        //   if(scholarship.length>0){
+        //     delete t.scholarships
+        //     t['scholarships'] = scholarship
+        //     filterData.push(t)
+        //      //filterData[index]['scholarships']=scholarship
+        //   }
+        //   else{
+        //   // console.log( filterData[index])
+        //   }
+        //            // console.log(filterData)
+       
+        // });
+        // }
+        // else {
+        //   this.details4.scholarships.filter((data, index) => {
+        //     if (data.content_shift.name == event.target.value) {
+        //       return filterData.push(data)
+        //     }
+        //     // this.details.scholarships = filterData
+        //   }
+        //   )
+        //   this.details3 = this.details.scholarships
+        // }
+
+      // }
+      
+      if (event.target.value != 'Ver Todos') {
+       
+       this.filterResults.map((t,index) => {    
+           var scholarships =  this.filterScholarships(t.scholarships);
+      // console.log(scholarships)        
+          if(scholarships.length>0){ 
+           this.filterResults[index]['scholarships'] = scholarships
           }
           else{
-          // console.log( filterData[index])
+             this.filterResults[index]['scholarships'] = []
           }
-                   // console.log(filterData)
-       
-        });
-        }
-        else {
-          this.details4.scholarships.filter((data, index) => {
-            if (data.content_shift.name == event.target.value) {
-              return filterData.push(data)
-            }
-            // this.details.scholarships = filterData
-          }
-          )
-          // this.details = this.details.filter((data, index) => {
-          //   return data.content_shift.name == event.target.value
-          // })
-          this.details3 = this.details.scholarships
-        }
+      });
 
       }
       else {
@@ -376,7 +420,7 @@ export default {
       }
     },
     modeChange2(event) {
-      this.filterResults = this.details
+      // this.filterResults = this.details
       if (event.target.value != 'Ver Todos') {
         this.status = true;
 
@@ -574,7 +618,8 @@ export default {
         </div> -->
       </div>
 
-      <div class="col-md-9" v-if="CS_DATA_LIST.length > 0">
+<div class="col-md-9" v-if="!filterActive">
+      <div  v-if="CS_DATA_LIST.length > 0">
         <div class="card institution" v-for="(data, index) in CS_DATA_LIST" :key='data'>
           <div class="card-body">
             <div class="row">
@@ -695,9 +740,135 @@ export default {
         </nav>
         </div> -->
       </div>
+      </div>
+      
+<div class="col-md-9"  v-if="filterActive">
+      <div  v-if="filterResults.length > 0">
+        <div class="card institution" v-for="(data, index) in filterResults" :key='data'>
+          <div class="card-body" v-if="data.scholarships.length>0" >
+            <div class="row">
+              <div class="col-md-3">
+                <div>
+                  <img :src="data.school?.image" height="100" class="imagess" />
+                  <div class="text-center mb-4">
+                    <h3>{{ data.school?.name }}</h3>
+                  </div>
+                  <!-- <div v-html="data.more_bonus_text_button">
+                  </div> -->
+
+                </div>
+              </div>
+              <div class="col-md-9 ead">
+                <h3>{{ data.course?.name }}</h3>
+                <div>
+                  <label class="mt-3">Modalities:</label> <br />
+
+                  <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+                    <div v-for="data in data.modalities" :key='data' class="mx-1">
+                      <input type="radio" class="btn-check" name="btnradio" v-bind:id="data.name + index"
+                        autocomplete="off">
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name + index"
+                        @click="changeModalities(data, index)">{{
+                            data.name
+                        }}</label>
+                    </div>
+                  </div>
+
+                  <br />
+                  <label class="mt-3">Shifts :</label> <br />
+                  <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group">
+                    <div v-for="data in data.shifts" :key='data' class="mx-1">
+                      <input type="radio" class="btn-check" name="btnradio1" v-bind:id="data.name + index"
+                        autocomplete="off">
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name + index"
+                        @click="changeShifts(data, index)">{{
+                            data.name
+                        }}</label>
+                    </div>
+                  </div>
+                  <br />
+                  <label class="mt-3">Units:</label> <br />
+                  <div class="btn-group" role="group" aria-label="Basic checkbox toggle button group"
+                    style="flex-wrap:wrap">
+                    <div v-for="data in data.units" :key='data' class="mx-1">
+                      <input type="radio" class="btn-check" name="btnradio2" v-bind:id="data.name + index"
+                        autocomplete="off">
+                      <label class="btn btn-outline-success btn-sm" v-bind:for="data.name + index"
+                        @click="changeUnits(data, index)">{{ data.name
+                        }}</label>
+                    </div>
+                  </div>
+
+                </div>
+              </div>
+            </div>
+
+            <div class="my-4">
+              <hr />
+            </div>
+            <div>
+              <div v-if="data.scholarships.length > 0">
+                <div class="payment" v-for="data1 in data.scholarships" :key='data1'>
+                  <div class="row">
+                    <div class="col-md-9">
+                      <div class="row">
+                        <div class="col-md-3">
+                          <p>Modality: <b> {{ data1?.content_modality?.name }} </b><br />
+                            Shift:<b> {{ data1?.content_shift?.name }} </b><br />
+                            Unit:<b>{{ data1?.unit?.name }} </b></p>
+                        </div>
+                        <div class="col-md-3">
+                          <p>Type: <b> {{ data1?.content_type?.name }} </b><br />
+                            's Degree Duration: <br /> <b>{{ data1?.duration }} {{ data1?.duration_type }} </b><br />
+                            Installments:<b> {{ data1?.discount_installments }} </b></p>
+                        </div>
+                        <div class="col-md-3">
+                          <p class="p-0">Monthly payment: <br />
+                            <span class="frombrl">From BRL {{ data1?.original_price }}</span><br />
+                            For <b> BRL <br /><span class="brl">{{ data1?.comission_amount }}</span> </b>
+                          </p>
+                          <small class="text-center">8785865 VB163.28 - 100</small>
+                        </div>
+                        <div class="col-md-3">
+                          <p>Scholarship: <b> {{ data1?.percent }}%</b> <br />
+                            Total Savings:
+                            <b> BRL {{ data1?.economy_total }} </b>
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                    <div class="col-md-3 text-center">
+                      <a href="#">see details</a> <br />
+                      <button class="btn btn-success btn-sm" type="button">CHOOSE THIS BAG</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div v-if="data.scholarships.length == 0">
+                <p>Não há bolsas disponíveis com os itens selecionados, mude a unidade, turno ou modalidade selecionada.
+                </p>
+              </div>
+            </div>
+
+          </div>
+        </div>
+        <!-- <div class="my-4">
+        <nav aria-label="Page navigation example">
+          <ul class="pagination">
+            <li class="page-item"><a class="page-link" href="#">Previous</a></li>
+            <li class="page-item"><a class="page-link" href="#">1</a></li>
+            <li class="page-item"><a class="page-link" href="#">2</a></li>
+            <li class="page-item"><a class="page-link" href="#">3</a></li>
+            <li class="page-item"><a class="page-link" href="#">Next</a></li>
+          </ul>
+        </nav>
+        </div> -->
+      </div>
+      </div>
     </div>
 
   </div>
+  
 </template>
 
 <style scoped>
